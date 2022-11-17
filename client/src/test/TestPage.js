f613a005c3fa227c07c7ade237d49e49fa50c219
kwanjung <kwanjungkim@naver.com>
2022-11-18 02:32:26 +0900

    react 에서 express로 json 전달

--- client/src/test/TestPage.js
+++ client/src/test/TestPage.js
@@ -1,22 +1,60 @@
 import { useState, useEffect } from "react";
 
 const TestPage = () => {
-    const [string, setString] = useState("enter");
+    const [string, setString] = useState("disconnect");
+    const [information,setInformation] = useState({
+        name : "",
+        userid : ""
+    });
+    const {name,userid} = information;
+
     useEffect(() => {
         fetch('http://localhost:8080/test')
         .then(res => {
             return res.json()
         })
-        .then(data => {
-            console.log(data);
-            setString(data.content);
+        .then(res => {
+            console.log(res);
+            setString(res.content);
         })
     }, [])
+
+    const contentSubmit = (e) => {
+        const req = {
+            method : "POST",
+            headers : {
+                "Content-type" : "application/json"
+            },
+            body : JSON.stringify({information})
+        };
+        console.log("req.body : "+req.body);
+
+        fetch('http://localhost:8080/testPost', req)
+        .then((res) => res.json())
+        .then((res) => {
+            console.log(res.content);
+        })
+
+        setInformation({
+            name:"",
+            userid:""
+        })
+    }
+
+    const onChange = (e) => {
+        const {name,value} = e.target;
+        setInformation({
+            ...information,
+            [name]:value
+        })
+    }
 
     return (<section>
         <h1>this is test page</h1>
         <p>{string}</p>
-        <input type="button"></input>
+        <input name="name" placeholder="name" onChange={onChange} value={name}></input><p></p>
+        <input name="userid" placeholder="userid" onChange={onChange} value={userid}></input>
+        <button type="submit" onClick={contentSubmit}>send</button>
     </section>)
 };
 
