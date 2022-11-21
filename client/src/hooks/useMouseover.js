33fa226acbaa23a2527c133f69601275fa5dfd4a
Wille Lee <1992season@gmail.com>
2022-11-22 01:06:01 +0900

    add comments on hooks

--- client/src/hooks/useMouseover.js
+++ client/src/hooks/useMouseover.js
@@ -1,5 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 
+/**
+ * This functions returns an object of a boolean state created by useState() and a reference created by useRef().
+ * Once a tag of the component is marked with the reference, the state turns into true when the mouse is over the HTML tag,
+ * otherwise, the state goes false.
+ * @returns {object}
+ */
 const useMouseover = () => {
   const [state, setState] = useState(false);
   const mouseoverRef = useRef();
