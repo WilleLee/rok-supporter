f96037f8578b31a026a9f9adc54b8fd922bf5328
Wille Lee <1992season@gmail.com>
2022-11-23 18:59:47 +0900

    boolean value on login controller

--- server/src/controller.js
+++ server/src/controller.js
@@ -1,50 +1,54 @@
-const mysql  = require('./rds');
+const mysql = require("./rds");
 mysql.connect();
 
 module.exports = {
-  needs : () => upload,
+  needs: () => upload,
 
-  api : {
-
-    test : (req,res) => {
-      res.json({content : 'now connected with server'})
+  api: {
+    test: (req, res) => {
+      res.json({ content: "now connected with server" });
     },
 
-    login : (req,res) => {
+    login: (req, res) => {
       console.log(req.body.password);
-      if(req.body.password == 'aaa') {
-        res.json({checkPw : 'true'});
+      if (req.body.password == "aaa") {
+        res.json({ checkPw: true });
       } else {
-        res.json({checkPw : 'false'});
+        res.json({ checkPw: false });
       }
     },
 
-    file : (req,res) => {
-      return res.sendFile(__dirname + "/build/index.html")
+    file: (req, res) => {
+      return res.sendFile(__dirname + "/build/index.html");
     },
 
-    testDb : (req,res) => {
+    testDb: (req, res) => {
       mysql.query("SELECT * FROM rok_supporter.testData;", (err, rows) => {
-        if(!err) {
+        if (!err) {
           res.send(rows);
         } else {
-          console.log('query error : ${err}');
+          console.log("query error : ${err}");
           res.send(err);
         }
       });
     },
 
-    testPost : (req,res) => {
-      mysql.query("SELECT name FROM rok_supporter.testData WHERE id = '" + req.body.information.userid+"';", (err, result) => {
-        if(!err) {
-          res.json(result);
-        } else {
-          console.log('query error : ${err}');
-          res.json(result);
+    testPost: (req, res) => {
+      mysql.query(
+        "SELECT name FROM rok_supporter.testData WHERE id = '" +
+          req.body.information.userid +
+          "';",
+        (err, result) => {
+          if (!err) {
+            res.json(result);
+          } else {
+            console.log("query error : ${err}");
+            res.json(result);
+          }
         }
-      });
+      );
 
-      console.log(" userid : "+req.body.information.userid);
-    }
-  }
-}
+      console.log(" userid : " + req.body.information.userid);
+    },
+  },
+};
