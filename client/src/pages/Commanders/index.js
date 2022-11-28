b05c269fc193f77423537921f2cdfac5c12b6a8d
Wille Lee <1992season@gmail.com>
2022-11-28 21:19:15 +0900

    edit commanders btns

--- client/src/pages/Commanders/index.js
+++ client/src/pages/Commanders/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import CommandersList from "../../components/CommandersList";
+import styles from "../../styles/pages/commanders.module.scss";
 
 const troopTypes = {
   $ARC: "archer",
@@ -11,10 +12,25 @@ const CommandersPage = () => {
   const [troopType, setTroopType] = useState(troopTypes.$ARC);
   return (
     <section>
-      <div>
-        <button onClick={() => setTroopType(troopTypes.$ARC)}>archer</button>
-        <button onClick={() => setTroopType(troopTypes.$INF)}>infantry</button>
-        <button onClick={() => setTroopType(troopTypes.$CAV)}>cavalry</button>
+      <div className={styles.troop_type_btns}>
+        <button
+          className={`${troopType === troopTypes.$ARC ? styles.active : ""}`}
+          onClick={() => setTroopType(troopTypes.$ARC)}
+        >
+          archer
+        </button>
+        <button
+          className={`${troopType === troopTypes.$INF ? styles.active : ""}`}
+          onClick={() => setTroopType(troopTypes.$INF)}
+        >
+          infantry
+        </button>
+        <button
+          className={`${troopType === troopTypes.$CAV ? styles.active : ""}`}
+          onClick={() => setTroopType(troopTypes.$CAV)}
+        >
+          cavalry
+        </button>
       </div>
       <h1>hi</h1>
       <CommandersList troopType={troopType} />
