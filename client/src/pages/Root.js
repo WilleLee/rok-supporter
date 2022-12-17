05e1e899c8ae17fa20b2ac835a0cf1cb44b1aa94
Wille Lee <1992season@gmail.com>
2022-12-17 17:04:38 +0900

    started useHomeImageSelected

--- client/src/pages/Root.js
+++ client/src/pages/Root.js
@@ -1,10 +1,21 @@
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
-import { useLangModeStore, useLoggedInStore } from "../store";
+import {
+  useHomeImageSelected,
+  useLangModeStore,
+  useLoggedInStore,
+} from "../store";
 import shallow from "zustand/shallow";
 import { useEffect } from "react";
 
 const Root = () => {
+  const { selectedImage, resetImageSelected } = useHomeImageSelected(
+    (state) => ({
+      selectedImage: state.selectedImage,
+      resetImageSelected: state.resetImageSelected,
+    }),
+    shallow
+  );
   const { loggedIn, logout } = useLoggedInStore(
     (state) => ({
       loggedIn: state.loggedIn,
@@ -32,14 +43,21 @@ const Root = () => {
       window.addEventListener("beforeunload", onBeforeUnload);
     }
   }, [loggedIn, logout]);
+  useEffect(() => {
+    if (selectedImage && selectedImage.length) {
+      document.querySelector("body").classList.add("fix");
+    } else {
+      document.querySelector("body").classList.remove("fix");
+    }
+  }, [selectedImage]);
   return (
-    <>
+    <main>
       <Header langMode={langMode} loggedIn={loggedIn} logout={logout} />
       <Outlet />
       <button onClick={changeLangMode} className="ct_langmode_btn">
         En / 한글
       </button>
-    </>
+    </main>
   );
 };
 
